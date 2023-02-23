fbd9f6bfb121780474b81ca6bfae43385735b82a
joellesenne <jfkles@me.com>
2023-02-23 22:04:54 +0100

    Fist commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: 'https://joellesenne.dev', title: 'Home' },
+  { href: 'https://joellesenne.dev/projets', title: 'Projects' },
+  { href: '/', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
